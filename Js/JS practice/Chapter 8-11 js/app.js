1790f1a0bd236244ef209e4019eb383a036c9ead
Ayanaanonaeem <ayanaano5@gmail.com>
2026-01-12 16:54:39 +0500

    JS chapter 8-11 completed

---
+++ Js/JS practice/Chapter 8-11 js/app.js
@@ -0,0 +1,71 @@
+// Concatenation
+var name="Ayan ";
+var lastName="Naeem";
+
+console.log(name+lastName);
+
+var abc="1"
+var cde="2"
+
+
+console.log(1+1+abc);
+
+// do string ke uper plus laagdo to woh concatenate hojatin hai 
+
+// agr hum variable main number as a string rakhen to aur plus lagaden to woh concatenate hojata hai 
+// lkn age oi aur operation lagayen to woh chal jayega
+
+// Prompt
+
+// var a=prompt("enter your age");
+// console.log(a);
+
+var userBirthYear=prompt("Enter your birth year")
+var currentYear=2026
+var userAge=currentYear-userBirthYear
+
+console.log(userAge);
+
+// for converting the string into number we have three ways number 1 is we can use plus before varaible 
+
+var hello="2"
+var newhello=hello
+
+// Or we can use method like NUmber or parseint or parse
+
+newhello=Number(hello)
+parseInt(hello)
+parseFloat(hello)
+console.log(typeof(newhello));
+
+
+
+// var userNum=+prompt("Enter a Number")
+// if (userNum%2==0) {
+//     console.log("Your number is even");
+
+    
+    
+// } else {
+//     console.log("Your number is odd");
+    
+// }
+
+// Comparison operator
+
+// == is used for comparing only 
+// === it also check datatype also 
+
+var username=prompt('Enter you name')
+var capitalizeUserName= username.toUpperCase()
+if (capitalizeUserName==="AYAN") {
+    console.log("Welcome ayan");
+    
+}if (capitalizeUserName!=="AYAN") {
+    console.log("welcome",username);
+    
+} else {
+    console.log("Welcome ayan");
+    
+}
+
