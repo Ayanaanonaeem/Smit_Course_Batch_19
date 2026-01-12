eb5d5f61ed414227ebb682f170991ad099c36f0e
Ayanaanonaeem <ayanaano5@gmail.com>
2026-01-12 16:20:39 +0500

    JS chapter 1-7 completed

---
+++ Js/JS practice/Chapter 1-7 js/app.js
@@ -0,0 +1,43 @@
+// alert("Hello World")
+console.log("Hi")
+
+// for declaring variable in js
+
+var a;
+a="ayan";
+console.log(a);
+
+// variable for numbers
+
+var age=20;
+age=30+20;
+console.log(age);
+
+// Maths operation
+ var a=2+2
+ var popularname=2;
+ var evennumbers=10;
+ console.log(popularname+evennumbers);
+
+//  Post increment practice
+ 
+var num=13
+num++ //13
+console.log(num++);//14
+
+var num2=14
+var num3=++num2 + num2++ + ++num2 + ++num2 + num2++
+          15    +  15     +  17   +   18    + 18
+          
+
+console.log(num3);
+
+
+// B O D M A S 
+
+console.log(2+2*3-4/4); //7
+console.log(8+3*4*2-4-3/1);//25
+
+
+
+ 
