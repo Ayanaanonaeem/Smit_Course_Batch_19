665af8ca82770ac622af2f01fdb6236e752bede6
Ayanaanonaeem <ayanaano5@gmail.com>
2026-02-19 05:00:18 +0500

    Js chapter 18,19,20 COMPLETED

---
+++ Js/JS practice/Chapter 18,19,20 js/app.js
@@ -0,0 +1,72 @@
+// for loop
+
+var array1 = [];
+
+for (let i = 0; i < 3; i++) {
+	var userValue = prompt("Enter your values");
+	array1.push(userValue);
+}
+console.log(array1);
+
+// for(let i=0; i<array1.length;i++){
+//     if (array1[i]=="anas") {
+//         console.log("anas found");
+//         break
+//     }
+//     else{
+//        console.log("anas is not in this array");
+
+//     }
+// }
+
+// chapter 19 flags bollean array length and loopus interuptus
+
+
+// this below example is called flag 
+
+var anasfound = false;
+for (let i = 0; i < array1.length; i++) {
+	if (array1[i] == "anas") {
+		anasfound = true;
+		console.log("Anas found");
+		break;
+	}
+}
+
+if ((anasfound = false)) {
+	console.log("anas is not found");
+}
+
+
+// flag one more example
+
+var cities=["karachi","islamabad","lahore","hyderabad"]
+var cityTocheck="karachi"
+
+var cityfound=false
+
+for(i=0; i<=cities.length; i++){
+    if (cities[i]==cityTocheck) {
+        cityfound=true
+        console.log("i found the city");
+        break
+        
+    }
+}
+if (cityfound==false) {
+    console.log("your city is not here");
+    
+}
+
+
+// for loop nested
+
+let n = 5;
+
+for (let i = 1; i <= n; i++) {     // rows
+  let row = "";
+  for (let j = 1; j <= i; j++) {   // stars depend on row
+    row += "*";
+  }
+  console.log(row);
+}
