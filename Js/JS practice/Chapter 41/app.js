5431ddb3c39ffc92eb8950440347cbadf0cdfa12
Ayanaanonaeem <ayanaano5@gmail.com>
2026-02-27 04:52:14 +0500

    Chapter 41,42 completed

---
+++ Js/JS practice/Chapter 41/app.js
@@ -0,0 +1,19 @@
+// -------------------While loop-----------------------------
+var i=0
+while(i<=10){
+    console.log(i);
+    i++
+}
+
+
+// -------------do while loop----------------------------------
+
+
+var num=1
+
+do {
+    console.log(num);
+    num++
+} while (num=0);
+
+// agr conditions false huwi tb bhi ek br lazmi chalega
