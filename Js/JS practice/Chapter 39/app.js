696d7b4aebd8d82b74d148a77cfd5cab1dd62be3
Ayanaanonaeem <ayanaano5@gmail.com>
2026-02-26 06:19:20 +0500

    Chapter 39 completed

---
+++ Js/JS practice/Chapter 39/app.js
@@ -0,0 +1,42 @@
+// -------------------------Chapter 39---------------------
+
+//  Switch and case statement
+
+var value = "ayan";
+switch (value) {
+case "jawaid":
+	console.log("this is jawaid");
+break;
+
+case "ayan":
+	console.log("here is ayan");
+break;
+
+default:
+	console.log("No one is Ayan");
+break;
+}
+
+
+var fruit="peach"
+
+switch(fruit){
+case "apple":
+    console.log("this is apple");
+break;
+
+case "banana":
+    console.log("this is banana");
+break;
+
+case "mango":
+    console.log("this is mango");
+break;
+
+default:
+    console.log("This is definitely a peach");
+break; 
+        
+}
+// This is mostly used when you have many conditions to check
+
