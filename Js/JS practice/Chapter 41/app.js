c686e84ba8084c907ae30d6f77818a65b65f2f50
Ayanaanonaeem <ayanaano5@gmail.com>
2026-02-27 04:55:20 +0500

    Chapter 41,42,43,44 completed

--- Js/JS practice/Chapter 41/app.js
+++ Js/JS practice/Chapter 41/app.js
@@ -17,3 +17,12 @@ do {
 } while (num=0);
 
 // agr conditions false huwi tb bhi ek br lazmi chalega
+
+// ---------------------------Chapter 44------------------------
+
+// commenting
+
+// For single line we have double slash
+// For multiline we have /* */
+
+/*var para="Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima architecto molestiae voluptatum vel non alias odit veritatis ipsum ex, sint reprehenderit sequi beatae!"*/
