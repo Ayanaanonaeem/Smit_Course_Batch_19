19524228d28dfa958b74b61ccfc43de65b9d3500
Ayanaanonaeem <ayanaano5@gmail.com>
2026-02-27 05:44:43 +0500

    Chapter 45,46,47 completed

---
+++ Js/JS practice/Chapter 45/app.js
@@ -0,0 +1,15 @@
+// ----------------------------Chapter 45------------------------/
+// is main hamne seekha ke html ke sth popup message kese show karwayen
+
+function greet(msg) {
+    alert(msg)
+}
+
+// ----------------------------Chapter 46 ------------------------/
+
+function welcome() {
+    alert("hello user")
+}
+
+
+// ----------------------------Chapter 47 ------------------------/
