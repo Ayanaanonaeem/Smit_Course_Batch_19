5bd80bca96196d1e2f9da53d15735f9d17bc64ca
Ayanaanonaeem <ayanaano5@gmail.com>
2026-03-04 04:10:42 +0500

    Chapter 55,56 completed

---
+++ Js/JS practice/Chapter 55/app.js
@@ -0,0 +1,27 @@
+var para=document.getElementById("para")
+var head=document.getElementById("head")
+// var head2= document.getComputedStyle("head")
+function big() {
+    para.className+=" big"
+    para.style.fontSize="60px"
+    head.style.visibility="hidden"
+}
+var head=document.getElementById("head").margin
+
+// ham properties ko read bhi krsakte hain ye uper wale syntax ko use krke
+
+
+
+
+// ------------------------chapter 56------------------------
+
+// we can target all the parapgraph together with tag name 
+
+var para=document.getElementsByTagName("p")
+
+
+var para1txt=para[0]
+para1txt.innerHTML="hello"
+
+
+// but ye hamen array main dega not in a single value 
