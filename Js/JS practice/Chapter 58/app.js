39dd77fd5f6db0c93e4bd66bfe55acc9ec31753f
Ayanaanonaeem <ayanaano5@gmail.com>
2026-03-05 03:40:58 +0500

    Chapter 58 DOM

---
+++ Js/JS practice/Chapter 58/app.js
@@ -0,0 +1,18 @@
+var a=document.querySelector("h1")
+
+console.log(a);
+ a.innerHTML="i will Finish my frontend this Year"
+ a.style .backgroundColor="black"
+
+
+var para=document.querySelector("p")
+
+para.addEventListener("click",function(){
+    para.style.color="white"
+    para.style.backgroundColor="yellow"
+})
+
+var btn=document.getElementById("btn")
+btn.addEventListener("click", function() {
+    
+})
