035ab83ef82f72dc2f3b953015a635f09dbd3f48
Ayanaanonaeem <ayanaano5@gmail.com>
2026-03-10 03:38:42 +0500

    Cricket Website

--- Js Projects/Cricket Website/app.js
+++ Js Projects/Cricket Website/app.js
@@ -5,7 +5,8 @@ let playerName;
 const secondDiv = document.getElementById("second");
 const para=document.getElementsByClassName("teams")
 let selectedTeam;
-
+const thirdDiv=document.getElementById("third")
+// const thirdDivPara=document.querySelectorAll(".toss")
 
 firstBtn.addEventListener("click", function () {
 	if (firstInput.value == "") {
@@ -21,10 +22,19 @@ for (let i = 0; i < para.length; i++) {
     para[i].addEventListener("click",function() {
        selectedTeam=para[i].innerText
        secondDiv.classList.replace("flex","none")
+	   thirdDiv.classList.replace("none","flex")
        
-        
+	   
     })
     
+}
+
+// alert("now it a time for toss chose one")
+
+var randomTossNumber=Math.ceil(Math.random()*2)
+
+function checkToss(params) {
+	
 }
 
 
