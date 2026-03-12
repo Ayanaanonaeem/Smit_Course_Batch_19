9f0646f0cacd8f99435623a034732d3764d18a7d
Ayanaanonaeem <ayanaano5@gmail.com>
2026-03-13 03:59:23 +0500

    Cricket Website

--- Js Projects/Cricket Website/app.js
+++ Js Projects/Cricket Website/app.js
@@ -19,16 +19,7 @@ let paragraph;
 let paragraph_2;        
 let paragraph_3;        
 
-let myobj={
-	score:0,
-	out:0,
-	target:0,
- 
-}
-const batting={
-	wicket:["bowled","lbw","caught","runout"],
-	score:[0,1,2,3,4,5,6]
-}
+
 firstBtn.addEventListener("click", function () {
 	if (firstInput.value == "") {
 		alert("please enter a value first");
@@ -36,24 +27,19 @@ firstBtn.addEventListener("click", function () {
 		playerName = firstInput.value;
 		firstDiv.classList.replace("flex", "none");
         secondDiv.classList.replace("none","flex")
-		 paragraph=document.createElement("p")
-         paragraph.textContent="Player name: "+playerName;
-	    fifthDiv.appendChild(paragraph)
-
+	
+		
         
 
 	}
 });
 
 for (let i = 0; i < para.length; i++) {
-    para[i].addEventListener("click",function() {
+	para[i].addEventListener("click",function() {
        selectedTeam=para[i].innerText
        secondDiv.classList.replace("flex","none")
 	   thirdDiv.classList.replace("none","flex")
-	    paragraph_2=document.createElement("p")
-        paragraph_2.textContent="Your Team: "+selectedTeam;
-	    fifthDiv.appendChild(paragraph_2)
-
+	
         
 	   
     })
@@ -72,7 +58,7 @@ var randomSelection=batBallArray[Math.ceil(Math.random()*2)]
 
 
 for (let i = 0; i < thirdDivPara.length; i++) {
-    thirdDivPara[i].addEventListener("click",function() {
+	thirdDivPara[i].addEventListener("click",function() {
        if (randomTossNumber==2) {
 		 alert("You won the toss now choose bat or ball")
 		 thirdDiv.classList.replace("flex","none")
@@ -94,19 +80,34 @@ var currentscore;
 var targetchild;
 var target;
 let numTarget;
+const para1=document.getElementById("sixthpara1")
+const para2=document.getElementById("sixthpara2")
+const para3=document.getElementById("sixthpara3")
+const para4=document.getElementById("sixthpara4")
+const batting={
+	wicket:["bowled","lbw","caught","runout"],
+	score:[0,1,2,3,4,5,6]
+}
 
-
+let myobj={
+	score:0,
+	out:0,
+	target:0,
+ 
+}
 bat.addEventListener("click",function() {
 	
 	userselection=bat.innerText
 	// console.log(userselection);
 	alert("You are batting first")	
-        
+   
+	
 	fourthDiv.classList.replace("flex","none")
 	fifthDiv.classList.replace("none","flex")
-	paragraph_3=document.createElement("p")
-	paragraph_3.textContent="You are "+userselection+"ing";
-	fifthDiv.appendChild(paragraph_3)
+	para3.innerText=userselection+"ting"
+	para1.innerText="Player name:"+playerName
+	para2.innerText="Selected Team:"+selectedTeam
+
 
    document.addEventListener("keypress",function (e) {
 	if (e.key==" ") {		
@@ -114,19 +115,19 @@ bat.addEventListener("click",function() {
 		wicketRandomNum=Math.floor(Math.random()*4)
 		wicketName=batting.wicket[wicketRandomNum]
 		if (scoreAndwicketRandomNum==0) {
-			currentWicket=Number(wicket.innerText)+1
-			wicket.innerText=currentWicket
+			myobj.out=myobj.out+1
+			wicket.innerText=myobj.out
+			console.log(myobj.out);
+			
 			alert("you are "+wicketName)
-			if (currentWicket==11) {
+			if (myobj.out==11) {
 				alert("So you are all out click okay for next innings")
 				wicket.innerText=0
-				target=score.innerText
-				numTarget=Number(target)+1
-
-				targetchild=document.createElement("h2")
-				targetchild.textContent="Target :"+numTarget
-				score.innerText=0
-				fifthDiv.appendChild(targetchild)
+				myobj.target=Number(score.innerText)+1
+                para4.innerText="Target: "+myobj.target
+				
+				
+				
 				// console.log(score.innerText);
 				
 				
@@ -139,6 +140,7 @@ bat.addEventListener("click",function() {
 			scoreName=batting.score[scoreRandomNum]
 			currentscore=Number(score.innerHTML)+scoreName
 			score.innerText=currentscore
+		    
 			alert("you have made "+scoreName+" run")
 		}
 		
@@ -159,10 +161,9 @@ bowl.addEventListener("click",function() {
 	alert("You are bowling first")	
 	fourthDiv.classList.replace("flex","none")
 	fifthDiv.classList.replace("none","flex")
-
-	paragraph_3=document.createElement("p")
-	paragraph_3.textContent="You are "+userselection+"ing";
-	fifthDiv.appendChild(paragraph_3)
+	para3.innerText=userselection+"ing"
+	para1.innerText="Player name:"+playerName
+	para2.innerText="Selected Team:"+selectedTeam
 
 	
 
