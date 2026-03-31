cab58ee84a7ff03d0c1687ec182ed69d10e50a4b
Ayanaanonaeem <ayanaano5@gmail.com>
2026-04-01 01:06:30 +0500

    dom fourth project completed

---
+++ Js Projects/Dom projects/Multiple image hovering animation/app.js
@@ -0,0 +1,21 @@
+var elem=document.querySelectorAll(".elem")
+
+elem.forEach((val)=>{
+console.log(val.childNodes[3]);
+
+val.addEventListener("mouseenter",function(){
+  val.childNodes[3].style.opacity="1"
+    
+    
+})
+val.addEventListener("mouseleave",function(){
+  val.childNodes[3].style.opacity="0"
+    
+    
+    
+})
+val.addEventListener("mousemove",function(dets){
+    val.childNodes[3].style.left=dets.x+"px"
+    // val.childNodes[3].style.top=dets.y+"px"
+})
+})
