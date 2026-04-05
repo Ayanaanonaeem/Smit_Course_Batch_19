356e1ad654a17a0a71ff39493aff0a2d2f877385
Ayanaanonaeem <ayanaano5@gmail.com>
2026-04-05 23:15:14 +0500

    object

---
+++ Js/JS practice/Objects/app.js
@@ -0,0 +1,34 @@
+// Object practice 
+
+var obj={
+    name:"Ayan",
+    lastName:"Naeem",
+    age:20,
+}
+
+var obj2={
+    name:"jawaid",
+    lastName:"iqbal",
+    
+}
+
+var obj3=obj
+obj3.salary="40"
+
+
+var obj4={
+    name:"Ayan",
+    studies:"Sir syed university of engineering and technology",
+    age:20,
+}
+
+console.log(obj4.name);
+console.log(obj4.studies);
+console.log(obj4.age);
+
+obj4.age="30"
+console.log(obj4);
+
+
+
+
