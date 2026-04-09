87abde0881dc963b06c7716777e089003ed0c9a8
Ayanaanonaeem <ayanaano5@gmail.com>
2026-04-09 22:13:19 +0500

    Let vs Const difference

---
+++ Js/Es6+/es6+.js
@@ -0,0 +1,32 @@
+console.log("es6+js");
+
+// let vs const difference 
+
+let a="String"
+a="Blue" //hum let ko reassign krsakte hain aese without using let
+
+console.log(a);
+
+{
+
+    let b=24
+    console.log(b);
+    
+}
+
+let c=10
+{
+    let c=20
+    console.log(c);
+    
+}
+console.log(c);
+
+ 
+
+// agr hamen value fix rakhni hai to hum const use krte hain 
+
+const name="ayan"
+// const name="shayan"   hum const ko reassign nhi krsakte 
+
+
