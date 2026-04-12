d81df21319f34ba4b417b4ad04f3b407125b7478
Ayanaanonaeem <ayanaano5@gmail.com>
2026-04-12 17:37:35 +0500

    Map method of array

---
+++ Js/Es6+/arraymethod.js
@@ -0,0 +1,19 @@
+
+// map is a method of an array ye apne under teen parameter leta hai number 1 Element number 2 uska 
+// index and number 3 pora array 
+
+// Ye hamen new array return krta hai
+
+let array=[1,2,3,4,5,6,7,8,]
+
+let newArray=array.map((ele)=>{
+
+    return ele+1
+ 
+
+})
+
+console.log(array);
+console.log(newArray);
+
+
