e26ccddfd97c74186cabf2d234d0ff69a4d874d7
Ayanaanonaeem <ayanaano5@gmail.com>
2026-04-14 03:07:11 +0500

    filter Method of an array

--- Js/Es6+/arraymethod.js
+++ Js/Es6+/arraymethod.js
@@ -1,3 +1,4 @@
+// **************************** MAP METHOD ***********************************
 
 // map is a method of an array ye apne under teen parameter leta hai number 1 Element number 2 uska 
 // index and number 3 pora array 
@@ -15,5 +16,22 @@ let newArray=array.map((ele)=>{
 
 console.log(array);
 console.log(newArray);
+
+
+// **************************** Filter METHOD ***********************************
+
+// Filter bhi hamen new array return krta hai value ko filter out krke unpr condition match krke 
+// and ye bhi teen parameter leta hai number 1 ele, number 2 index of that element ,number 3 whole array
+
+
+let newArray2=array.filter((ele)=>{
+   
+    return ele%2==0
+
+})
+
+console.log(newArray2);
+
+
 
 
