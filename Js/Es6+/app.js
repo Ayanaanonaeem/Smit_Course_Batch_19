6cd778eb9ff41fdb193069f214b608bf626306f5
Ayan Naeem <ayanaano5@gmail.com>
2026-06-15 23:25:19 +0500

    Few changes

--- Js/Es6+/app.js
+++ Js/Es6+/app.js
@@ -2,6 +2,8 @@ console.log("app.js");
 console.log("hello");
 console.log("how are you doing today");
 console.log("What about you");
+console.log("What about you");
+console.log("What about you");
 
 
 
