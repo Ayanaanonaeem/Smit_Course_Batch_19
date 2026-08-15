358c5e4eb01dfa58d53696f6541f087f1b209950
Ayan Naeem <ayanaano5@gmail.com>
2026-08-15 23:41:37 +0500

    Few practice

--- Js/Es6+/es6+.js
+++ Js/Es6+/es6+.js
@@ -307,6 +307,7 @@ console.log("hello this is my code");
 console.log("Hello my name is Ayan")
 console.log ("hi this is my laptop which get hang some time")
 console.log ("hi this is my laptop which get hang some time")
+console.log ("Hello")
 
 
 
