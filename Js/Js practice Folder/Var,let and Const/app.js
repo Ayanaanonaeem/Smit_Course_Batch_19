666cdd25b8d1ed6c2bee956551dfdab753044005
Ayan Naeem <ayanaano5@gmail.com>
2026-08-19 03:53:28 +0500

    var,let and const practice

---
+++ Js/Js practice Folder/Var,let and Const/app.js
@@ -0,0 +1,51 @@
+
+// 666666666666666666 var 666666666666666666666666666666
+
+// var is a global scope variable
+var count;  //declare: varibale bnana
+var count=10 //initializa :value dena first time
+var count=10 //reassign dobara value dena 
+
+var glow="land"
+{
+    glow="iceland"
+}
+
+console.log(glow);
+
+
+// 666666666666666666 let 666666666666666666666666666666
+
+let name="ayan"
+name ="jawaid"  //reassign krsakte hain krsakte hain
+
+
+// blocked sccope hai 
+
+// let name ="fraz" //redeclare nahi krsakte error dega 
+// console.log(let);
+
+let gender="female"
+{
+  let   gender="male"
+      
+}
+   
+// console.log(blue());
+ 
+
+console.log(gender);
+
+
+
+
+
+
+
+//###############  const  ########################
+
+// const age;// isko initialize krna lazmi hai sirf declare nahi krsakte ise 
+// console.log(age);
+
+const age=12// ye sahi hai
+
