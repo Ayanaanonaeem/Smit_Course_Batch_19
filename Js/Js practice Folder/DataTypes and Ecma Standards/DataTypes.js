fb2507adf8705bd6c320a38545904211bb8a45c1
Ayan Naeem <ayanaano5@gmail.com>
2026-08-23 06:12:00 +0500

    JS practice done

---
+++ Js/Js practice Folder/DataTypes and Ecma Standards/DataTypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat all js code as newer version
+
+// alert(3+3)  // we can not run this in node js
+
+
+// code readability should be high 
+
+console.log(3+3); console.log(3+3)
+
+console.log(typeof(null));//object ayega 
+
+console.log(typeof(undefined));//undefined ayega 
+
+
+// 7 type of datatype
+/*
+1. Number 
+2. null
+3. boolean
+4. bigInt
+5. Symbol
+6. String
+7. undefined
+*/
+// NNBBSSU  short form to remember this 
+
+
+
