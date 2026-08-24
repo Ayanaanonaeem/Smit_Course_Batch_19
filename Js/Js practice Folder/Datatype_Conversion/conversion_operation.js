257d222b434890cfa0dfc860e5b27e1374089f42
Ayan Naeem <ayanaano5@gmail.com>
2026-08-24 18:17:32 +0500

    js practice

---
+++ Js/Js practice Folder/Datatype_Conversion/conversion_operation.js
@@ -0,0 +1,46 @@
+let score = true
+// console.log(typeof score);
+
+let convertedScore=Number(score)
+// console.log(convertedScore);
+// console.log(typeof convertedScore);
+
+
+// agr hum number main string likh den aur uski type dekhen to number hojati hai woh
+// but real main nan number hoti hai 
+
+// hamne dekha ke undefined ko number main convert kren to woh wese nan hai lkn uski type number hai
+
+// aur agr hum null ko covert kren to woh 0 hojata hai aur uski type bhi numbeer hai 
+
+
+// agr hum bollean value ko number main convert kren to true 1 hoga aur false 0 hoga 
+
+
+let number=1
+let convertedNumber=Boolean(number)
+
+console.log(convertedNumber);
+console.log(typeof convertedNumber);
+
+
+// aur is trah hum number ko bollean main convert krsakte hain
+
+// kuch falsy value hoti hain js main jo sirf false return krtin hain 
+
+let number2=45
+let convertedNumber2=String(number2)
+
+
+console.log(convertedNumber2);
+console.log(typeof convertedNumber2);
+
+
+
+// aur number ko stirng main convert krsakte hain 
+
+
+
+
+
+
