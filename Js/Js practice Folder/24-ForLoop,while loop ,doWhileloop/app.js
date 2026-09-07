515738486380b0e6c4ee035218e0a320a0054ca9
Ayan Naeem <ayanaano5@gmail.com>
2026-09-08 00:16:50 +0500

    for loop and switch case practice

---
+++ Js/Js practice Folder/24-ForLoop,while loop ,doWhileloop/app.js
@@ -0,0 +1,63 @@
+// for (let i = 0; i < 10; i++) {
+//     const element = i
+//     console.log(element);
+    
+    
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+   
+    
+//     if(i == 5){
+//         console.log("5 found");
+//         break
+//     }
+//     console.log(i);
+    
+// }
+// for (let i = 0; i < 10; i++) {
+   
+    
+//     if(i == 5){
+//         console.log("5 found");
+//         continue
+//     }
+//     console.log(i);
+    
+// }
+
+
+// while loop 
+
+// let i=0
+// while (i<=10) {
+//     console.log(i);
+//     i++
+    
+// }
+
+// do while always run one time 
+let i=0
+do {
+    console.log(i);
+    i++
+} while (i<=10);
+
+
+// switch case statement 
+switch ("mango") {
+    case "banana":
+        console.log("banana");
+        
+        break;
+    case "mango":
+        console.log("mango");
+        
+        break;
+
+    default:
+        console.log("apple");
+        
+        break;
+}
